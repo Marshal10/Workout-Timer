0b1b76797e2cdda8dfd92f5e7394d0e65491c0ef
Marshal10 <marshalmaleane123@gmail.com>
2024-08-24 15:23:55 +0530

    live time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSound from "./ToggleSound";
 
+function formatTime(date) {
+  return new Intl.DateTimeFormat("en", {
+    month: "short",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
+
 function App() {
+  const [time, setTime] = useState(formatTime(new Date()));
+
+  useEffect(function () {
+    const id = setInterval(function () {
+      setTime(formatTime(new Date()));
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
   return (
     <main>
       <h1>Workout Timer</h1>
-      <time>For your workout on X</time>
+      <time>For your workout on {time}</time>
       <ToggleSound />
       <Calculator />
     </main>
